a9eac959e6fdc81e1ccf6aeb521c8477af95dc5a
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-05 22:34:17 +0530

    render a list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,17 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ['tag1', 'tag2', 'tag3']
   };
 
   render() {
-    let classes = this.getBadgeClasses();
     return (
-      <div className="d-flex">
-        <span className={classes}>{this.formatCount()}</span>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
       </div>
     );
   }
